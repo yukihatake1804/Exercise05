33b0db45a42dfbdc35d152e8742567eb1842c16e
hieu <nhokhieuthao@gmail.com>
2019-10-08 17:25:22 +0700

    FinishProblem1

---
+++ Exercise5a/Exercise5a/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise5a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintAtoZ();
+            PrintAtoZ2();
+        }
+
+        static void PrintAtoZ()
+        {
+            for (int i = 65;i < 91; i++)
+            {
+                Console.Write((char)i + "  ");
+            }
+            Console.WriteLine(" ");
+        }
+        static void PrintAtoZ2()
+        {
+            for (int i = 90; i > 64; i--)
+            {
+                Console.Write((char)i + "  ");
+            }
+            Console.WriteLine(" ");
+        }
+        static void PrintAtoZ3()
+        {
+
+        }
+
+    }
+}
